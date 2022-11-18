25e87af6741ef4656c112afeb2e9f47a6203a019
Augustinchini <a.chini@cilea.fr>
2022-11-18 10:12:18 +0100

    feat: in memory data source

--- packages/2-paradigm/app/adapters/secondary/inMemory/inMemoryCharacterDataSource.ts
+++ packages/2-paradigm/app/adapters/secondary/inMemory/inMemoryCharacterDataSource.ts
@@ -1,3 +1,5 @@
+import CharacterNotFoundExecption from "packages/2-paradigm/app/core/exceptions/characterNotFoundExeception"
+import Character from "../../../core/models/character/character"
 import CharacterDto from "../../../core/models/character/dto/characterDto"
 import ICharacterDataSource from "../../../core/useCases/character/interfaces/ICharacterDataSource"
 
@@ -14,5 +16,16 @@ export default class InMemoryCharacterDataSource
     public create(characterDto: CharacterDto): Promise<void> {
         this.charactersList.push(characterDto)
         return Promise.resolve()
+    }
+
+    /**
+     * Get a caracter by id
+     * @param id character id
+     * @returns {Promise<void>}
+     */
+    public read(id: string): Promise<Character> {
+        const character = this.charactersList.find((c) => c.id === id)
+        if (character) return Promise.resolve(new Character(character))
+        throw new CharacterNotFoundExecption()
     }
 }
